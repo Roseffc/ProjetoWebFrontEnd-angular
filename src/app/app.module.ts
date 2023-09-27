85ed48ca73054e80431793b57ec79e8b445c1609
Rosangela <roseffc@gmail.com>
2023-09-26 21:26:42 -0300

    feat: criado component total-cad e add classes utilitárias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { TotalCardComponent } from './components/total-card/total-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TotalCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/total-card/total-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-total-card',
+  templateUrl: './total-card.component.html',
+  styleUrls: ['./total-card.component.scss']
+})
+export class TotalCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
