5c8be5730bbe58a0442965337986e6b39eecd266
Rosangela <roseffc@gmail.com>
2023-09-27 19:26:00 -0300

    style: ajustes no component total-card.

--- src/app/components/total-card/total-card.component.ts
+++ src/app/components/total-card/total-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-total-card',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./total-card.component.scss']
 })
 export class TotalCardComponent implements OnInit {
+  @Input() title = '';
+  @Input() value = '';
 
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
