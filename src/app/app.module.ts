39b842b2f249a1569a70a5c264d93bc1ccd90e83
Rosangela <roseffc@gmail.com>
2023-09-29 22:20:24 -0300

    refactor: add dependencias module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,34 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { TotalCardComponent } from './components/total-card/total-card.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatError, MatFormFieldModule } from '@angular/material/form-field';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import {MatSelectModule} from '@angular/material/select';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatListModule} from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatListModule } from '@angular/material/list';
 import { TransactionListComponent } from './components/transaction-list/transaction-list.component';
 import { TransactionService } from './services/transaction.service';
 import { HttpClientModule } from '@angular/common/http';
-import {MatTableModule} from '@angular/material/table';
-import {MatSortModule} from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { CommonModule, CurrencyPipe } from '@angular/common';
+import { PieChartComponent } from './components/pie-chart/pie-chart.component';
+import { ColumnChartComponent } from './components/column-chart/column-chart.component';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatNativeDateModule} from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePtBr from '@angular/common/locales/pt';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { SharedStateService } from './services/shared-state.service';
+
+registerLocaleData(localePtBr);
 
 
 @NgModule({
@@ -27,7 +43,9 @@ import { CommonModule, CurrencyPipe } from '@angular/common';
     AppComponent,
     TotalCardComponent,
     DashboardComponent,
-    TransactionListComponent
+    TransactionListComponent,
+    PieChartComponent,
+    ColumnChartComponent
   ],
   imports: [
     BrowserModule,
@@ -35,7 +53,6 @@ import { CommonModule, CurrencyPipe } from '@angular/common';
     BrowserAnimationsModule,
     MatButtonModule,
     MatCardModule,
-    MatFormFieldModule,
     MatSidenavModule,
     MatSelectModule,
     MatButtonToggleModule,
@@ -44,10 +61,17 @@ import { CommonModule, CurrencyPipe } from '@angular/common';
     MatTableModule,
     MatSortModule,
     MatPaginatorModule,
-    CommonModule
-
+    CommonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    FormsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    NgApexchartsModule
   ],
-  providers: [TransactionService, CurrencyPipe],
+  providers: [TransactionService, CurrencyPipe, SharedStateService, { provide: LOCALE_ID, useValue: 'pt-BR' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
